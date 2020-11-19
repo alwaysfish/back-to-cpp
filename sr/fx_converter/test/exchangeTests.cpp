8034fcda0df759fd859772eb2044a3db287f4cf0
StephenRipley <stephen_ripley@hotmail.com>
2020-11-19 15:44:12 +0000

    updated tests to work correctly

--- sr/fx_converter/test/exchangeTests.cpp
+++ sr/fx_converter/test/exchangeTests.cpp
@@ -2,10 +2,11 @@
 #define BOOST_TEST_MODULE GRNN test suite
 #include <boost/test/unit_test.hpp>
 #include <vector>
+#include <iostream>
 
 #include "currencyList.hpp"
 #include "exchangeBoard.hpp"
-/*
+
 BOOST_AUTO_TEST_CASE(testThatCurrenciesAreReadFromFile)
 {
     vector<Currency> currencies;
@@ -13,20 +14,21 @@ BOOST_AUTO_TEST_CASE(testThatCurrenciesAreReadFromFile)
     BOOST_CHECK_EQUAL(currencies.size(), 81); //hardcoded, change to variable later
 }
 
+
 BOOST_AUTO_TEST_CASE(testThatAValidBaseMustBeEntered)
 {
     ExchangeBoard exchangeBoard;
     exchangeBoard.readExchangeRatesFromFile("/home/stephen/projects/back-to-cpp/datasets/exchange_rates.csv");
-    double convertedAmount = exchangeBoard.exchange("UDS", "GBP", 0); //Incorrect base returns amount + error
-    BOOST_CHECK(convertedAmount == 0.0);
+    double convertedAmount = exchangeBoard.exchange("UDS", "GBP", 0);
+    BOOST_CHECK(convertedAmount == -1);
 }
 
 BOOST_AUTO_TEST_CASE(testThatAValidQuoteMustBeEntered)
 {
     ExchangeBoard exchangeBoard;
     exchangeBoard.readExchangeRatesFromFile("/home/stephen/projects/back-to-cpp/datasets/exchange_rates.csv");
-    double convertedAmount = exchangeBoard.exchange("USD", "GP", 0); //Incorrect quote returns amount + error
-    BOOST_CHECK(convertedAmount == 0.0);
+    double convertedAmount = exchangeBoard.exchange("USD", "GP", 0);
+    BOOST_CHECK(convertedAmount == -1);
 }   
 
 BOOST_AUTO_TEST_CASE(testThatADirectConversionCanBeMade)
@@ -34,13 +36,10 @@ BOOST_AUTO_TEST_CASE(testThatADirectConversionCanBeMade)
     ExchangeBoard exchangeBoard;
     exchangeBoard.readExchangeRatesFromFile("/home/stephen/projects/back-to-cpp/datasets/exchange_rates.csv");
     double convertedAmount = exchangeBoard.exchange("GBP", "USD", 100); 
-    BOOST_CHECK(convertedAmount == 137.01);
+    double expectedAmount = 100*1.3701; //hardcoded, change to amount*find->second.getBid
+    BOOST_CHECK(convertedAmount == expectedAmount);
 }
-*/
 
-BOOST_AUTO_TEST_CASE(test1)
-{
-    BOOST_CHECK_EQUAL(1,1);
-}
+
 //testThatAnIntermediaryConversionCanBeMade
 //testThatExchangeRatesCanBeReadFromFile
